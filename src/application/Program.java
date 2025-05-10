24666037b6aeab3883ea2b0b2469120953981f2d
fmacedosantos <felipemacedo986@gmail.com>
2025-05-10 15:58:14 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,5 +41,9 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n==== TEST 6: seller delete ====");
+        sellerDao.deleteById(9);
+        System.out.println("Delete completed");
     }
 }
